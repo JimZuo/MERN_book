e8e770b0277982c33d3088068e1adca88f650166
JimZuo <1057491653@qq.com>
2022-04-01 01:27:55 +0800

    My first commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/api/books', books);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT, '0.0.0.0');
 
 // Accessing the path module
 const path = require("path");
