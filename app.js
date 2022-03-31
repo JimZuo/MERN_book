8f346c9d6692de31fea37334edb1cda570cd1159
JimZuo <1057491653@qq.com>
2022-04-01 00:48:00 +0800

    My first commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // Accessing the path module
-//const path = require("path");
+const path = require("path");
 
 // Step 1:
 app.use(express.static(path.resolve(__dirname, "./mern_book/build")));
